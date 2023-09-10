642bfb5b7ea3632f9dab45f66e060ab27ca9178b
Wainainajoyce <joycewainaina19@gmail.com>
2023-09-10 15:09:59 +0300

    Changed a few files

---
+++ domicilecode/script.js
@@ -0,0 +1,20 @@
+document.getElementById('login-btn').addEventListener('click', function() {
+    window.location.href = 'rooms.html'; 
+});
+document.getElementById('signup-btn').addEventListener('click', function() {
+    window.location.href = 'account.html'; 
+});
+document.getElementById('login-roommate-btn').addEventListener('click', function() {
+    window.location.href = 'login.html'; 
+});
+document.getElementById('login-landlord-btn').addEventListener('click', function() {
+    window.location.href = 'loginlandlord.html'; 
+});
+document.getElementById('login-btn1').addEventListener('click', function() {
+    window.location.href = 'landlorddashboard.html'; 
+});
+document.getElementById('property-info').addEventListener('click', function() {
+    window.location.href = ''; 
+});
+
+
